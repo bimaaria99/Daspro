03a9bb0b4ff95b50c58e257781d20079d1ef8550
bimaaria99 <bimaaria382@gmail.com>
2026-08-27 19:19:26 +0700

    Add files via upload

---
+++ DASPRO/Project/latihan09.java
@@ -0,0 +1,9 @@
+public class latihan09 {
+     public static void main (String[] args){
+        System.out.println("Nama saya Davinza Aria Bima");
+        System.out.println("Belajar Java sangat menyenangkan");
+        System.out.println("Semakin Sering Mencoba Semakin Mahir");
+        System.out.print("Belajar java Tidaklah Sulit, yang sulit adalah malas untuk belajar");
+        //System.out.print("Teruslah Berlatih Dan Jangan Pernah Menyerah");
+    }   
+}

416ef589d96a77224ce05768353b63873aeef6ef
bimaaria99 <bimaaria382@gmail.com>
2026-09-23 09:13:47 +0700

    Add files via upload

---
+++ DASPRO/Tugas_teori/DASPRO/Project/Jobsheet02/ContohPenggunaanOperator.java
@@ -0,0 +1,21 @@
+public class ContohPenggunaanOperator {
+
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+        int c = 2;
+
+        System.out.println("Artimetika Operator");
+        System.out.println("Bilangan a adalah : " + a);
+        System.out.println("Bilangan b adalah : " + b);
+        System.out.println("Bilangan c adalah : " + c);
+        System.out.println("Hasil a + b + c = " + (a + b + c));
+        System.out.println("Hasil a - b - c = " + (a - b - c));
+        System.out.println("Hasil a * b * c = " + (a * b));
+        System.out.println("Hasil a / b / c = " + (a / b));
+        System.out.println("Hasil a % b % c = " + (a % b));
+        System.out.println("Hasil a + b + c = " + (a+b*c));
+        System.out.println("Hasil a - b - c = " + ((a+b)*c));
+    }
+    
+}
---
+++ DASPRO/Tugas_teori/DASPRO/Tugas_teori/Pertemuan03/GajiKaryawan09.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class GajiKaryawan09 {
+    public static void main(String[] args) {
+        int gajiPokok;
+        double bonus;
+        double tunjTransp=60000;
+        double tunjMkn=40000;
+        
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Masukkan gaji pokok: ");
+
+        gajiPokok = sc.nextInt();
+        bonus = 0.05 * gajiPokok;
+        double totalGaji = gajiPokok + bonus + tunjTransp + tunjMkn - (0.01 * gajiPokok);
+        
+        System.out.println("Bonus bulanan anda adalah Rp. " + bonus);
+        System.out.println("gaji yang anda terima adalah Rp. " + totalGaji);
+        sc.close();
+    }
+}

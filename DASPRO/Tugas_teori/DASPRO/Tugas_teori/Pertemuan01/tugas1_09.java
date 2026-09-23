435fc887305e913a87f9b559e735deaa31514a3b
bimaaria99 <bimaaria382@gmail.com>
2026-09-23 09:15:11 +0700

    Delete DASPRO/Tugas_teori/DASPRO/Tugas_teori directory

--- DASPRO/Tugas_teori/DASPRO/Tugas_teori/Pertemuan01/tugas1_09.java
+++
@@ -1,9 +0,0 @@
-public class tugas1_09 {
-    public static void main(String[] args) {
-
-        System.out.println("Setelah langkah 1: Nampan A - kosong, Nampan B - Bulan, Nampan C - Bintang");
-        System.out.println("Setelah langkah 1: Nampan A - Bulan, Nampan B - kosong, Nampan C - Bintang");
-        System.out.println("Setelah langkah 1: Nampan A - Bulan, Nampan B - Bintang, Nampan C - kosong");
-        System.out.println("Kesimpulan: Pertanyaan yang benar adalah (A) dan (C)");
-    }
-}

0fe5b66b0d174bcd336d8d039659b7f660fa389d
bimaaria99 <bimaaria382@gmail.com>
2026-09-23 09:15:22 +0700

    Delete DASPRO/Tugas_teori/DASPRO directory

--- DASPRO/Tugas_teori/DASPRO/Project/Jobsheet 01/latihan09.java
+++
@@ -1,9 +0,0 @@
-public class latihan09 {
-     public static void main (String[] args){
-        System.out.println("Nama saya Davinza Aria Bima");
-        System.out.println("Belajar Java sangat menyenangkan");
-        System.out.println("Semakin Sering Mencoba Semakin Mahir");
-        System.out.println("Belajar java Tidaklah Sulit, yang sulit adalah malas untuk belajar");
-        System.out.print("Teruslah Berlatih Dan Jangan Pernah Menyerah");
-    }   
-}
